cca1accd96ca54cd301e69f707b298d855bd6384
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-08-12 17:52:03 +0300

    get data from firestore and listen for auth state changes

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,17 +1,58 @@
-// sign up
+// get data
+db.collection('/guides')
+  .get()
+  .then((snapshot) => {
+    setupGuides(snapshot.docs);
+  });
 
+// listen for auth state changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log('user logged in', user);
+  } else {
+    console.log('user logged out');
+  }
+});
+
+// sign up
 const signupForm = document.querySelector('#signup-form');
 
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  // get user info
 
+  // get user info
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
+
   // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+  auth.createUserWithEmailAndPassword(email, password).then(() => {
+    // close the sign up modal and reset form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
+  });
+});
+
+//log out
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+// log in
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(() => {
+    // close the login modal and reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
   });
 });
